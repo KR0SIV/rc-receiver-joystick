70cf28a450799ef4e9c2edf0b0623a23fdd91d8c
Corrosive <harold@giddings.me>
2018-07-24 16:16:27 -0400

    Translated comments to english

--- hidRcJoy.ino
+++ hidRcJoy.ino
@@ -1,55 +1,55 @@
-/*
-Conector para receptor de RC a USB
-Emula Joystick HID para poder usar el transmisor de 2 canales en simuladores de RC
+/ *
+Connector for RC to USB receiver
+Emulate Joystick HID to use the 2-channel transmitter in RC simulators
 
-Requerimientos:
-Placa Digispark con ATTINY85
-Parche para usar interrupciones diferentes en los pines de IO que no son USB https://github.com/J-Rios/Digispark_DigiUSB-SoftSerial
-Realizar un jumper entre los pines 2 y 3 del digispark para habilitar el dispositivo USB
-Dudas? www.patolin.com/receptor-rc-digispark
-*/
+Requirements:
+Digispark plate with ATTINY85
+Patch to use different interrupts in non-USB IO pins https://github.com/J-Rios/Digispark_DigiUSB-SoftSerial
+Perform a jumper between pins 2 and 3 of digispark to enable the USB device
+Doubts? www.patolin.com/receptor-rc-digispark
+* /
 
 
-#include <SoftRcPulseIn.h>  // lectura sin bloqueo de pulsos PPM
-#include <TinyPinChange.h>  // requerido para SoftRcPulseIn.h
-#include "DigiJoystick.h"   // libreria Joystick HID Digispark
+#include <SoftRcPulseIn.h> // reading without pulse block PPM
+#include <TinyPinChange.h> // required for SoftRcPulseIn.h
+#include "DigiJoystick.h" // HID Digispark Joystick Library
 
-// pines usados en el digispark
-#define CHANNEL1_PIN   0
-#define CHANNEL2_PIN   1
-#define CHANNEL3_PIN   5  // opcional para transmisor de 3 canales
+// pins used in the digispark
+#define CHANNEL1_PIN 0
+#define CHANNEL2_PIN 1
+#define CHANNEL3_PIN 5 // optional for 3-channel transmitter
 
-// Creamos los objetos SoftRcpulseInOut para leer los pulsos PPM
-SoftRcPulseIn  Channel_1_Pulse;
-SoftRcPulseIn  Channel_2_Pulse;
-SoftRcPulseIn  Channel_3_Pulse;
+// Create the SoftRcpulseInOut objects to read the PPM pulses
+SoftRcPulseIn Channel_1_Pulse;
+SoftRcPulseIn Channel_2_Pulse;
+SoftRcPulseIn Channel_3_Pulse;
 
-// Valores de posicion del transmisor 
+// Transmitter position values
 byte chan1val;
 byte chan2val;
 byte chan3val;
 
 
-void setup()
+void setup ()
 {
-  // asignamos cada pin a su objeto de lectura correspondiente
-  Channel_1_Pulse.attach(CHANNEL1_PIN);
-  Channel_2_Pulse.attach(CHANNEL2_PIN);
-  Channel_3_Pulse.attach(CHANNEL3_PIN);
+  // we assign each pin to its corresponding reading object
+  Channel_1_Pulse.attach (CHANNEL1_PIN);
+  Channel_2_Pulse.attach (CHANNEL2_PIN);
+  Channel_3_Pulse.attach (CHANNEL3_PIN);
 }
 
-void loop()
+void loop ()
 {
-  // realizamos un mapeo de valores, para ajustar del rango PPM (1000-2000ms) a un byte de datos para el joystick (0-255)
-  chan1val=map(Channel_1_Pulse.width_us(), 700, 2300, 0, 255);
-  chan2val=map(Channel_2_Pulse.width_us(), 700, 2300, 0, 255);
-  chan3val=map(Channel_3_Pulse.width_us(), 700, 2300, 0, 255);
+  // we perform a mapping of values, to adjust the PPM range (1000-2000ms) to one byte of data for the joystick (0-255)
+  chan1val = map (Channel_1_Pulse.width_us (), 700, 2300, 0, 255);
+  chan2val = map (Channel_2_Pulse.width_us (), 700, 2300, 0, 255);
+  chan3val = map (Channel_3_Pulse.width_us (), 700, 2300, 0, 255);
 
-  // asignamos los valores al joystick
-  DigiJoystick.setX((byte) (chan1val));
-  DigiJoystick.setY((byte) (chan2val));
-  DigiJoystick.setXROT((byte) (chan3val));
+  // assign the values ​​to the joystick
+  DigiJoystick.setX ((byte) (chan1val));
+  DigiJoystick.setY ((byte) (chan2val));
+  DigiJoystick.setXROT ((byte) (chan3val));
 
-  // actualizamos los valores del joystick
-  DigiJoystick.delay(10); // retardo de 10ms requerido por la libreria USB
+  // update the joystick values
+  DigiJoystick.delay (10); // 10ms delay required by the USB library
 }
